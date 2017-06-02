daea3d995162b69b91fdf80121b20da17fd42e83
Wang Mu <nanqiao15@126.com>
2017-06-02 21:14:41 +0800

    add class: LSTMCell

--- RNNCell.py
+++ RNNCell.py
@@ -1,32 +1,33 @@
 import tensorflow as tf, numpy as np
 from tensorflow.python.util import nest
 from tensorflow.python.ops.rnn_cell_impl import _RNNCell as RNNCell
+from tensorflow.contrib.rnn import LSTMStateTuple
 from TFCommon.Initializer import gaussian_initializer, random_orthogonal_initializer
 from six.moves import xrange
 
  
 class GRUCell(RNNCell):
-    """Gated Recurrent Unit Cell, which recept an additional peak or context tensor."""
+    """Gated Recurrent Unit (GRU) recurrent network cell."""
 
     def __init__(self, num_units, init_state=None, reuse=None):
-        self._num_units = num_units
-        self._init_state = init_state
-        self._reuse = reuse
+        self.__num_units = num_units
+        self.__init_state = init_state
+        self.__reuse = reuse
 
     @property
     def state_size(self):
-        return self._num_units
+        return self.__num_units
 
     @property
     def output_size(self):
-        return self._num_units
+        return self.__num_units
 
     def zero_state(self, batch_size, dtype):
         return tuple([super(GRUCell, self).zero_state(batch_size, dtype)])
 
     def init_state(self, batch_size, dtype):
-        if self._init_state is not None:
-            return tuple([self._init_state])
+        if self.__init_state is not None:
+            return tuple([self.__init_state])
         else:
             return tuple([self.zero_state(batch_size, dtype)])
 
@@ -40,31 +41,24 @@ class GRUCell(RNNCell):
             if input_size is None:
                 raise ValueError("Expecting input_size to be set.")
 
-            # Check num_units == state_size from h_prev.
-            #num_units = h_prev.get_shape().with_rank(2)[1]
-            #if num_units != self._num_units:
-                #raise ValueError("Shape of h_prev[1] incorrect: num_units %i vs %s" %
-                                 #(self._num_units, num_units))
-
-            #if num_units is None:
-                #raise ValueError("num_units from `h_prev` should not be None.")
-
-            ### get weights for concated tensor.
-            W_shape = (input_size, self.state_size)
+            ### get weights.
+            W_shape = (input_size, self.output_size)
+            U_shape = (self.output_size, self.output_size)
+            b_shape = (self.output_size,)
             Wz = tf.get_variable(name='Wz', shape=W_shape)
             Wr = tf.get_variable(name='Wr', shape=W_shape)
             Wh = tf.get_variable(name='Wh', shape=W_shape)
-            Uz = tf.get_variable(name='Uz', shape=(self.state_size, self.state_size),
+            Uz = tf.get_variable(name='Uz', shape=U_shape,
                                  initializer=random_orthogonal_initializer())
-            Ur = tf.get_variable(name='Ur', shape=(self.state_size, self.state_size),
+            Ur = tf.get_variable(name='Ur', shape=U_shape,
                                  initializer=random_orthogonal_initializer())
-            Uh = tf.get_variable(name='Uh', shape=(self.state_size, self.state_size),
+            Uh = tf.get_variable(name='Uh', shape=U_shape,
                                  initializer=random_orthogonal_initializer())
-            bz = tf.get_variable(name='bz', shape=(self.state_size,),
+            bz = tf.get_variable(name='bz', shape=b_shape,
                                  initializer=tf.constant_initializer(0.0))
-            br = tf.get_variable(name='br', shape=(self.state_size,),
+            br = tf.get_variable(name='br', shape=b_shape,
                                  initializer=tf.constant_initializer(0.0))
-            bh = tf.get_variable(name='bh', shape=(self.state_size,),
+            bh = tf.get_variable(name='bh', shape=b_shape,
                                  initializer=tf.constant_initializer(0.0))
 
             ### calculate r and z
@@ -78,6 +72,72 @@ class GRUCell(RNNCell):
             new_h = (1-z) * h_prev + z * h_slash
 
             return new_h, tuple([new_h])
+
+class LSTMCell(RNNCell):
+    """Long Short-Term Memory (LSTM) unit recurrent network cell."""
+
+    def __init__(self, num_units, forget_bias=1.0, reuse=None):
+        self.__num_units = num_units
+        self.__forget_bias = forget_bias
+        self.__reuse = reuse
+
+    @property
+    def state_size(self):
+        return LSTMStateTuple(self.output_size, output_size)
+
+    @property
+    def output_size(self):
+        return self.__num_units
+
+    def __call__(self, x, state_prev, scope=None):
+        with tf.variable_scope(scope or type(self).__name__):
+
+            h_prev, c_prev = state_prev
+
+            # Check if the input size exist.
+            input_size = x.shape.with_rank(2)[1].value
+            if input_size is None:
+                raise ValueError("Expecting input_size to be set.")
+
+            ### get weights for concated tensor.
+            W_shape = (input_size, self.output_size)
+            U_shape = (self.output_size, self.output_size)
+            b_shape = (self.output_size,)
+            Wi = tf.get_variable(name='Wi', shape=W_shape)
+            Wj = tf.get_variable(name='Wj', shape=W_shape)
+            Wf = tf.get_variable(name='Wf', shape=W_shape)
+            Wo = tf.get_variable(name='Wo', shape=W_shape)
+            Ui = tf.get_variable(name='Ui', shape=U_shape,
+                                 initializer=random_orthogonal_initializer())
+            Uj = tf.get_variable(name='Uj', shape=U_shape,
+                                 initializer=random_orthogonal_initializer())
+            Uf = tf.get_variable(name='Uf', shape=U_shape,
+                                 initializer=random_orthogonal_initializer())
+            Uo = tf.get_variable(name='Uo', shape=U_shape,
+                                 initializer=random_orthogonal_initializer())
+            bi = tf.get_variable(name='bi', shape=b_shape,
+                                 initializer=tf.constant_initializer(0.0))
+            bj = tf.get_variable(name='bj', shape=b_shape,
+                                 initializer=tf.constant_initializer(0.0))
+            bf = tf.get_variable(name='bf', shape=b_shape,
+                                 initializer=tf.constant_initializer(self.__forget_bias)) # forget gate bias := 1
+            bo = tf.get_variable(name='bo', shape=b_shape,
+                                 initializer=tf.constant_initializer(0.0))
+
+            ### calculate gates and input's info
+            i = tf.tanh(tf.matmul(x, Wi) + tf.matmul(h_prev, Ui) + bi)
+            j = tf.sigmoid(tf.matmul(x, Wj) + tf.matmul(h_prev, Uj) + bj)
+            f = tf.sigmoid(tf.matmul(x, Wf) + tf.matmul(h_prev, Uf) + bf)
+            o = tf.tanh(tf.matmul(x, Wo) + tf.matmul(h_prev, Uo) + bo)
+
+            ### calculate candidate
+            new_c = f * c_prev + i * j
+
+            ### final cal
+            new_h = o * tf.tanh(new_c)
+
+            return new_h, tuple([new_h, new_c])
+
 
 
 class ResidualWrapper(RNNCell):
