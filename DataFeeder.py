c5819e8468b5132e7d8596601153baa9ffe33c03
Mu Wang <nanqiao15@126.com>
2017-09-25 23:52:28 +0800

    add validation support for BaseFeeder

--- DataFeeder.py
+++ DataFeeder.py
@@ -10,7 +10,7 @@ class BaseFeeder(threading.Thread):
         pre_process_batch,
         split_strategy (if split_nums is not None) methods.
     """
-    def __init__(self, coordinator, session, placeholders, key_lst, batch_size=32, split_nums=None):
+    def __init__(self, coordinator, session, placeholders, key_lst, batch_size=32, split_nums=None, is_validation=False):
         """
         :param coordinator:
         :param placeholders:
@@ -26,12 +26,16 @@ class BaseFeeder(threading.Thread):
             self.fed_holders[idx].set_shape(placeholders[idx].shape)
         self._placeholders = placeholders
         self.sess = session
+        assert isinstance(key_lst, list) or isinstance(key_lst, tuple)
         self.key_lst = key_lst
         self.batch_size = batch_size
         self.split_bool = False if split_nums is None else True
         self.split_nums = split_nums
+        assert isinstance(is_validation, bool)
+        self.is_validation = is_validation
         self._total_samples = len(key_lst)
         self._record_index = 0
+        self._loss = 0.
 
     def read_by_key(self, key):
         pass
@@ -42,11 +46,32 @@ class BaseFeeder(threading.Thread):
     def split_strategy(self, many_records) -> collections.Iterator:
         pass
 
+    def collect_loss(self, loss):
+        self._loss += loss
+
+    def mean_loss(self):
+        m_l = self._loss * self.batch_size / self._total_samples
+        self._loss = 0.
+        return m_l
+
     def prepare_batch(self):
         if not self.split_bool:
             self.feed_single_batch(self.fetch_one_batch())
         else:
             many_records = [self.fetch_one_record() for _ in xrange(self.batch_size * self.split_nums)]
+            for batch in self.split_strategy(many_records):
+                self.feed_single_batch(self.pre_process_batch(batch))
+
+    def prepare_validation(self):
+        if not self.split_bool:
+            while self._record_index <= (self._total_samples - self.batch_size):
+                self.feed_single_batch(self.fetch_one_batch())
+            remain_batch = []
+            while self._record_index != 0:
+                remain_batch.append(self.fetch_one_record())
+            self.feed_single_batch(self.pre_process_batch(remain_batch))
+        else:
+            many_records = [self.fetch_one_record() for _ in xrange(self._total_samples)]
             for batch in self.split_strategy(many_records):
                 self.feed_single_batch(self.pre_process_batch(batch))
 
@@ -73,7 +98,10 @@ class BaseFeeder(threading.Thread):
     def run(self):
         try:
             while not self.coord.should_stop():
-                self.prepare_batch()
+                if not self.is_validation:
+                    self.prepare_batch()
+                else:
+                    self.prepare_validation()
         except Exception as e:
             # Report exceptions to the coordinator.
             print('Data feeder thread failed.')
